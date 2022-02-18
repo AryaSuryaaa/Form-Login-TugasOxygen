b2f35a6ee622afc2ec078db02b990cf74290d116
AryaSuryaaa <97458994+AryaSuryaaa@users.noreply.github.com>
2022-02-18 21:52:17 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 const usernameSaved = "Username";
 const passwordSaved = "Password";
 
+const fName = "First Name";
+const lName = "Last Name";
+
 // Registrasi
 
 function data(){
@@ -10,10 +13,17 @@ function data(){
     const usernameRegis = document.getElementById("username-regis").value;
     const passwordRegis = document.getElementById("password-regis").value;
 
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+
     localStorage.setItem(usernameSaved, usernameRegis);
     localStorage.setItem(passwordSaved, passwordRegis);
 
+    localStorage.setItem(fName, firstName);
+    localStorage.setItem(lName, lastName);
+
     alert("your account has been created")
+    location.href = "index.html";
 };
 
 // login
@@ -28,12 +38,47 @@ function check() {
 
     if(usernameLogin.value == getUsername && passwordLogin.value == getPassword) {
         alert("You are logined");
+
+        location.href = "menu.html";
     } else {
-        alert("Username atau kata sandi salah")
+        alert("Incorrect Username or Password!")
     }
 };
 
+const getFirstName = localStorage.getItem(fName);
+const getLastName = localStorage.getItem(lName);
 
-document.getElementById("cantNotif").addEventListener("click", function() {
+const text = document.getElementById("text");
+text.innerText = "Hey! Welcome " + getFirstName +" "+ getLastName;
+
+document.getElementById("cantNotif1").addEventListener("click", function() {
     alert("fungsi ini belum tersedia");
-})
+});
+
+document.getElementById("cantNotif2").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif3").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif4").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif5").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif6").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif7").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
+
+document.getElementById("cantNotif8").addEventListener("click", function() {
+    alert("fungsi ini belum tersedia");
+});
