2933e78a9e2c9194daf90415ef4a12a8d2d69cdb
AryaSuryaaa <97458994+AryaSuryaaa@users.noreply.github.com>
2022-02-19 13:32:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,34 +51,3 @@ const getLastName = localStorage.getItem(lName);
 const text = document.getElementById("text");
 text.innerText = "Hey! Welcome " + getFirstName +" "+ getLastName;
 
-document.getElementById("cantNotif1").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif2").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif3").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif4").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif5").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif6").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif7").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
-
-document.getElementById("cantNotif8").addEventListener("click", function() {
-    alert("fungsi ini belum tersedia");
-});
